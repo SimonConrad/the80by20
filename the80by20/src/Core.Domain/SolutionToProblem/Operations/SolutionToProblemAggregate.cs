fd5b073055333f3bc4caae6c71a941266f70dc30
Szymon Kondratowicz <sk>
2022-07-27 22:03:41 +0200

    aggregate same if have same id

--- the80by20/src/Core.Domain/SolutionToProblem/Operations/SolutionToProblemAggregate.cs
+++ the80by20/src/Core.Domain/SolutionToProblem/Operations/SolutionToProblemAggregate.cs
@@ -5,7 +5,7 @@ using Core.Domain.SolutionToProblem.Capabilities;
 namespace Core.Domain.SolutionToProblem.Operations
 {
     [AggregateDdd]
-    public class SolutionToProblemAggregate : BaseEntity
+    public class SolutionToProblemAggregate : BaseEntity, IEquatable<SolutionToProblemAggregate>
     {
         protected SolutionToProblemAggregate() // INFO for EF purpose
         {
@@ -116,6 +116,26 @@ namespace Core.Domain.SolutionToProblem.Operations
             }
 
             WorkingOnSolutionEnded = true;
+        }
+
+        public bool Equals(SolutionToProblemAggregate? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SolutionToProblemAggregate)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
         }
     }
 }
