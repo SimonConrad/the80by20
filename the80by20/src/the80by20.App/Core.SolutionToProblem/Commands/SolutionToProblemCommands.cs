5ab4234251d4148d464333a9a12eac22ef8fd86c
Szymon Kondratowicz <sk>
2022-07-30 14:51:02 +0200

    implment rest commands from es-model of solution, but without handlers

--- the80by20/src/the80by20.App/Core.SolutionToProblem/Commands/SolutionToProblemCommands.cs
+++ the80by20/src/the80by20.App/Core.SolutionToProblem/Commands/SolutionToProblemCommands.cs
@@ -1,12 +1,41 @@
 ﻿using MediatR;
 using the80by20.Domain.ArchitectureBuildingBlocks;
+using the80by20.Domain.Core.SolutionToProblem.Capabilities;
 using the80by20.Domain.Core.SolutionToProblem.Operations.Problem;
 using the80by20.Domain.Core.SolutionToProblem.Operations.Solution;
+using the80by20.Domain.SharedKernel.Capabilities;
 
 namespace the80by20.App.Core.SolutionToProblem.Commands;
 
 [CommandDdd]
-public sealed record StartWorkingOnSolutionCommand(ProblemId ProblemId) : IRequest<SolutionToProblemId>;
+public sealed record StartWorkingOnSolutionCommand(ProblemId ProblemId) 
+    : IRequest<SolutionToProblemId>;
 
 [CommandDdd]
-public sealed record SetBasePriceOfSolutionCommand(SolutionToProblemId SolutionToProblemId) : IRequest<SolutionToProblemId>;
+public sealed record SetBasePriceOfSolutionCommand(SolutionToProblemId SolutionToProblemId) 
+    : IRequest<SolutionToProblemId>;
+
+// todo handler
+[CommandDdd]
+public sealed record AddSolutionElementCommand(SolutionToProblemId SolutionToProblemId, SolutionElement SolutionElement) 
+    : IRequest<SolutionToProblemId>;
+
+// todo handler
+[CommandDdd]
+public sealed record RemoveSolutionElementCommand(SolutionToProblemId SolutionToProblemId, SolutionElement SolutionElement) 
+    : IRequest<SolutionToProblemId>;
+
+// todo handler
+[CommandDdd]
+public sealed record SetSummaryCommand(SolutionToProblemId SolutionToProblemId, SolutionSummary SolutionSummary) 
+    : IRequest<SolutionToProblemId>;
+
+// todo handler
+[CommandDdd]
+public sealed record SetAdditionalPriceCommand(SolutionToProblemId SolutionToProblemId, Money additionalPrice) 
+    : IRequest<SolutionToProblemId>;
+
+// todo handler
+[CommandDdd]
+public sealed record FinishSolutionCommand(SolutionToProblemId SolutionToProblemId) 
+    : IRequest<SolutionToProblemId>;
