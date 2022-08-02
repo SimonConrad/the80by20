e42a5bebcae42226ab367f073cd3a1829421c91a
Szymon Kondratowicz <sk>
2022-08-02 10:32:00 +0200

    opened for extension closed to modificaiton sOlid principle done using by delgate passed to method

---
+++ the80by20/tools/the80by20.Sandbox/ExtensionPointUsingDelegate.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Authentication.ExtendedProtection;
+using Shouldly;
+using the80by20.Sandbox;
+
+namespace the80by20.Sandbox;
+
+// TODO do design patterns tests fpor exercising their usage, include spcification desgin pattern
+
+public class ExtensionPointUsingDelegate
+{
+    [Fact]
+    void Do()
+    {
+        var client = new StringBuilderExtendableClient();
+        var res = client.Do("base");
+
+        res.ShouldBe("base extended");
+    }
+}
+
+public abstract class AStringBuilderExtendableClient
+{
+    public string Do(string baseString)
+    {
+        var res = StringBuilderExtendable.Do(baseString, Extend);
+        return res;
+    }
+
+    protected virtual string Extend(string input)
+    {
+        return input;
+    }
+}
+
+public static class StringBuilderExtendable
+{
+
+    public static string Do(string baseString, Func<string, string> extends)
+    {
+        string extendedBase = extends?.Invoke(baseString);
+
+        return extendedBase;
+    }
+}
+
+public class StringBuilderExtendableClient : AStringBuilderExtendableClient
+{
+    protected override string Extend(string input)
+    {
+        return $"{input} extended";
+    }
+}
+
