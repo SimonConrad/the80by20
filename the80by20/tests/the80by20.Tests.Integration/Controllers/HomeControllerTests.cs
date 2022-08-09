14ffab52219abd37f8427edd1f86cb1ca23dbfe5
Szymon Kondratowicz <sk>
2022-08-09 14:26:42 +0200

    .

--- the80by20/tests/the80by20.Tests.Integration/Controllers/HomeControllerTests.cs
+++ the80by20/tests/the80by20.Tests.Integration/Controllers/HomeControllerTests.cs
@@ -5,12 +5,12 @@ namespace the80by20.Tests.Integration.Controllers;
 
 public class HomeControllerTests : ControllerTests
 {
-    //[Fact]
+    [Fact]
     public async Task get_base_endpoint_should_return_200_ok_status_code_and_api_name()
     {
         var response = await Client.GetAsync("/api");
         var content = await response.Content.ReadAsStringAsync();
-        content.ShouldBe("\"The 80 by 20 [test]\"");
+        content.ShouldContain("\"The 80 by 20 [test]\"");
     }
 
     public HomeControllerTests(OptionsProvider optionsProvider) : base(optionsProvider)
