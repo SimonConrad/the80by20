578adec052c56136343916c3b1fe09671769e029
Szymon Kondratowicz <sk>
2022-08-09 14:43:25 +0200

    .

--- the80by20/tests/the80by20.Tests.Integration/Controllers/HomeControllerTests.cs
+++ the80by20/tests/the80by20.Tests.Integration/Controllers/HomeControllerTests.cs
@@ -1,19 +1,42 @@
-﻿using Shouldly;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
 using Xunit;
 
 namespace the80by20.Tests.Integration.Controllers;
 
 public class HomeControllerTests : ControllerTests
 {
+    private SqliteConnection _connection;
+    private IWithCoreDbContext _testDatabase;
+
     [Fact]
     public async Task get_base_endpoint_should_return_200_ok_status_code_and_api_name()
     {
         var response = await Client.GetAsync("/api");
         var content = await response.Content.ReadAsStringAsync();
-        content.ShouldContain("The 80 by 20"); // todo sprawdzic czy testy na ci/cd githubas actoins na pewno używaja appsettings.automatictests
+        content.ShouldBe("\"The 80 by 20 [test]\"");
     }
 
     public HomeControllerTests(OptionsProvider optionsProvider) : base(optionsProvider)
     {
+    }
+
+    protected override void ConfigureServices(IServiceCollection services)
+    {
+        ApplySqlLite(services);
+    }
+
+    private void ApplySqlLite(IServiceCollection services)
+    {
+        var components = SqlLiteIneMemoryComponentsSetupper.Setup(services);
+        _connection = components.connection;
+        _testDatabase = components.ctxt;
+    }
+
+    public void Dispose()
+    {
+        _testDatabase.Dispose();
+        _connection.Dispose();
     }
 }
--- the80by20/tests/the80by20.Tests.Integration/Controllers/ProblemControllerTests.cs
+++ the80by20/tests/the80by20.Tests.Integration/Controllers/ProblemControllerTests.cs
@@ -17,6 +17,10 @@ namespace the80by20.Tests.Integration.Controllers;
 
 public class ProblemControllerTests : ControllerTests, IDisposable
 {
+
+    private IWithCoreDbContext _testDatabase;
+    private SqliteConnection _connection;
+
    // [Fact]
     public async Task post_problem_should_return_201_problem_created_and_data_is_persisted_in_write_and_read_store()
     {
@@ -71,9 +75,7 @@ public class ProblemControllerTests : ControllerTests, IDisposable
         }
     }
 
-    private IWithCoreDbContext _testDatabase;
 
-    private SqliteConnection _connection;
 
     public ProblemControllerTests(OptionsProvider optionsProvider) : base(optionsProvider)
     {
