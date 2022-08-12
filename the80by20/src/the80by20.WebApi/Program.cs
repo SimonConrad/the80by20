41b5da856961d03ad59e23882bb2fece3a21087b
Szymon Kondratowicz <sk>
2022-08-12 08:59:50 +0200

    tools commands

--- the80by20/src/the80by20.WebApi/Program.cs
+++ the80by20/src/the80by20.WebApi/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
@@ -13,46 +12,49 @@ builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
-
-builder.Host.UseSerilog((context, loggerConfiguration) =>
-{
-    //todo make working thath serilog read from logging levels are overriden by this what we have in appseetings (this is choden by environment)
-    // todo configure logger to read from appsettings.json
-    loggerConfiguration
-        .MinimumLevel.Information()
-        .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information);
-    //"Microsoft.EntityFrameworkCore.Database.Command": "Warning",
-    //"Microsoft.EntityFrameworkCore.Infrastructure": "Warning"
-
-    loggerConfiguration
-        .WriteTo
-        .Console();
-    //info can be output template
-
-    loggerConfiguration
-        .Enrich.FromLogContext()
-        .WriteTo.File(builder.Configuration.GetValue<string>("LogFilePath"),
-            rollingInterval: RollingInterval.Day,
-            rollOnFileSizeLimit: true,
-            retainedFileCountLimit: 10,
-            fileSizeLimitBytes: 4194304, // 4MB
-            //restrictedToMinimumLevel: LogEventLevel.Information,
-            outputTemplate:
-            "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({SourceContext}.{Method}) {Message}{NewLine}{Exception}");
-
-    // todo .{Method} is not logging method name
-    //loggerConfiguration.ReadFrom.Configuration(builder.Configuration);
-
-    // .WriteTo
-    // .File("logs.txt")
-    // .WriteTo
-    // .Seq("http://localhost:5341"); //todo kibana
-});
+ConfigureLogging(builder);
 
 var app = builder.Build();
-await app.UseInfrastructure(builder.Configuration);
 
+await app.UseInfrastructure(builder.Configuration);
 
 app.MapGet("api", (IOptions<AppOptions> options) => Results.Ok(options.Value.Name));
 
 app.Run();
+
+void ConfigureLogging(WebApplicationBuilder webApplicationBuilder)
+{
+    webApplicationBuilder.Host.UseSerilog((context, loggerConfiguration) =>
+    {
+        // todo make  serilog read from appseettings.ENV.json so that logging levels are overriden by this what we have in appseetings (based on application environment)
+        loggerConfiguration
+            .MinimumLevel.Information()
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information);
+        //"Microsoft.EntityFrameworkCore.Database.Command": "Warning",
+        //"Microsoft.EntityFrameworkCore.Infrastructure": "Warning"
+
+        loggerConfiguration
+            .WriteTo
+            .Console();
+        //info can use outputTemplate, like below
+
+        loggerConfiguration
+            .Enrich.FromLogContext()
+            .WriteTo.File(webApplicationBuilder.Configuration.GetValue<string>("LogFilePath"),
+                rollingInterval: RollingInterval.Day,
+                rollOnFileSizeLimit: true,
+                retainedFileCountLimit: 10,
+                fileSizeLimitBytes: 4194304, // 4MB
+                //restrictedToMinimumLevel: LogEventLevel.Information,
+                outputTemplate:
+                "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({SourceContext}.{Method}) {Message}{NewLine}{Exception}");
+
+        // todo .{Method} is not logging method name
+        //loggerConfiguration.ReadFrom.Configuration(builder.Configuration);
+
+        // .WriteTo
+        // .File("logs.txt")
+        // .WriteTo
+        // .Seq("http://localhost:5341"); //todo kibana, pod³aczyæ appinsights
+    });
+}
