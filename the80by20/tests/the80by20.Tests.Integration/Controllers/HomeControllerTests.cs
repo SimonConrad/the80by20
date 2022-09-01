eed62d5b59c07f030776a405e5c55e4b816a4bc4
Szymon Kondratowicz <sk>
2022-09-01 08:31:44 +0200

    test dobles info

--- the80by20/tests/the80by20.Tests.Integration/Controllers/HomeControllerTests.cs
+++ the80by20/tests/the80by20.Tests.Integration/Controllers/HomeControllerTests.cs
@@ -5,6 +5,26 @@ using Xunit;
 
 namespace the80by20.Tests.Integration.Controllers;
 
+
+// INFO Test Doubles (by Martin Fowler):
+//
+// Dummy - objects are passed around but never actually used. Usually they are just used to fill parameter lists.
+//
+// Fake - objects usually have working implmentations, but usually take same shortcut which makes
+// them not suitable for production (an in-memory-test-database is a good example)
+//
+// Stubs - provide canned answers to calls made during the test,
+// usually not responding at all to anything outside what is programmed in for the test.
+//
+// Spies - are stubs that also record some information based on how they were called.
+// One form of this might be an email service that records how many messages it was sent.
+//
+// Mocks - are pre-programmed with exceptions which form a specificaiton of the calls are expected to receive,
+// they can throw an exception if they receive a call they do not expect and are checked during verification to ensure
+// they got all the calls they were expecting.
+
+
+
 public class HomeControllerTests : ControllerTests
 {
     private SqliteConnection _connection;
