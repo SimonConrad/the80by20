dbad80b6fa6e2e9690775319a41d0bc65a55f158
Szymon Kondratowicz <sk>
2022-09-30 06:46:50 +0200

    .

--- the80by20/fe/the80by20/src/app/auth/auth.module.ts
+++ the80by20/fe/the80by20/src/app/auth/auth.module.ts
@@ -1,5 +1,8 @@
-import { NgModule } from '@angular/core'; // TODO consider instead importing SharedModule ?
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'; // TODO consider instead importing SharedModule ?
+
+
+import { Routes, RouterModule } from '@angular/router';
 
 import { AuthComponent } from './auth.component';
 import { RegisterComponent } from './register/register.component';
@@ -10,6 +13,17 @@ import { AuthGuard } from './auth.guard';
 
 // INFO based on https://appdividend.com/2022/02/02/angular-authentication/
 
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
+];
+
 @NgModule({
   declarations: [
     AuthComponent, // TODO check if it should be declared
@@ -17,8 +31,10 @@ import { AuthGuard } from './auth.guard';
     LoginComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ],
+  exports: [RouterModule],
   providers: [AuthService, AuthGuard]
 })
 export class AuthModule { }
