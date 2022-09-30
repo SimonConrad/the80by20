e25080c54ae4f21070fa32bede8497523b8c7317
Szymon Kondratowicz <sk>
2022-09-30 07:51:12 +0200

    .

--- the80by20/fe/the80by20/src/app/layout/header/header.component.ts
+++ the80by20/fe/the80by20/src/app/layout/header/header.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(public auth: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/auth/login'], {queryParams: {loggedOut: 'success'}});
   }
-
 }
