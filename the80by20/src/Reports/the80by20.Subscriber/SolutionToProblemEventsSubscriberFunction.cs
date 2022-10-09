06b937903beee4ea5b9868b687bd609a4a3c0921
simonconrad <szymon.kondratowicz@outlook.com>
2022-10-09 08:34:51 +0200

    azure function service bus tigger

--- the80by20/src/Reports/the80by20.Subscriber/SolutionToProblemEventsSubscriberFunction.cs
+++ the80by20/src/Reports/the80by20.Subscriber/SolutionToProblemEventsSubscriberFunction.cs
@@ -1,46 +1,23 @@
 using System;
-using System.Text;
-using Microsoft.Azure.ServiceBus;
-using Microsoft.Azure.ServiceBus.Core;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace the80by20.Subscriber
 {
-    // INFO https://www.mitrais.com/news-updates/handle-azure-service-bus-messages-queue-using-azure-functions/
+    // INFO https://learn.microsoft.com/en-us/azure/azure-functions/functions-bindings-service-bus-trigger?tabs=in-process%2Cextensionv5
     public class SolutionToProblemEventsSubscriberFunction
     {
         [FunctionName("SolutionToProblemEventsSubscriberFunction")]
-        public void Run([ServiceBusTrigger("%MessageQueue Name%", Connection = "ServiceBusConnectionString")]string msg, 
-        /*Message message,*/ /*MessageReceiver messageReceiver*/ ILogger log)
+        public void Run([ServiceBusTrigger("%MessageQueue Name%", Connection = "ServiceBusConnectionString")]string myQueueItem,
+            Int32 deliveryCount,
+            DateTime enqueuedTimeUtc,
+            string messageId,
+            ILogger log)
         {
-            try
-            {
-                //string payload = Encoding.UTF8.GetString(message.Body);
-               // log.LogInformation($"C# ServiceBus queue  trigger function processed message: {payload}");
-
-                log.LogInformation($"C# ServiceBus queue  trigger function processed message: {msg}");
-
-                //MyMessageModel model = JsonConvert.DeserializeObject<MyMessageModel>(payload);
-
-                //Do your things here, such as Some Actions or Calls Some Service or Another Method
-
-
-                //complete the message if there is no error
-                //messageReceiver.CompleteAsync(message.SystemProperties.LockToken);
-
-            }
-            catch (Exception)
-            {
-                // Do your error handling here
-
-
-                // Send message to DeadLetter Queue 
-                //messageReceiver.DeadLetterAsync(message.SystemProperties.LockToken);
-
-            }
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
+            log.LogInformation($"DeliveryCount={deliveryCount}");
+            log.LogInformation($"MessageId={messageId}");
         }
     }
 }
