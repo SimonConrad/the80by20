6df76beaa088d67401edc2c4933197031fd80f21
simonconrad <szymon.kondratowicz@outlook.com>
2022-10-09 14:17:28 +0200

    .

--- the80by20/src/Reports/the80by20.Subscriber/SolutionToProblemEventsSubscriberFunction.cs
+++ the80by20/src/Reports/the80by20.Subscriber/SolutionToProblemEventsSubscriberFunction.cs
@@ -8,16 +8,22 @@ namespace the80by20.Subscriber
     public class SolutionToProblemEventsSubscriberFunction
     {
         [FunctionName("SolutionToProblemEventsSubscriberFunction")]
-        public void Run([ServiceBusTrigger("%MessageQueue Name%", Connection = "ServiceBusConnectionString")]string myQueueItem,
+        public void Run([ServiceBusTrigger("%MessageQueue Name%", Connection = "ServiceBusConnectionString")]string message,
             Int32 deliveryCount,
             DateTime enqueuedTimeUtc,
             string messageId,
             ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {message}");
             log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
             log.LogInformation($"DeliveryCount={deliveryCount}");
             log.LogInformation($"MessageId={messageId}");
+
+
+            // TODO deserialize event (message function parameter) into model, persist it in denormalized table,
+            // table is inside dedicated db schema but same database for whole sytem,
+            // reports module has reading part - web api
+            // this table will be for querying most popular problem categories report (analytic readmodel)
         }
     }
 }
