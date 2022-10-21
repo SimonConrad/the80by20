81570d67618734d427590e28767ea06c599f2c9e
simonconrad <szymon.kondratowicz@outlook.com>
2022-10-21 07:33:33 +0200

    separate files for sevicecollection extensions and applicationbuilder extensions

---
+++ the80by20/src/Modules/Masterdata/the80by20.Masterdata.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace the80by20.Masterdata.Api.Controllers
+{
+    [Route("master-data")]
+    internal class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => "Master Data API";
+    }
+}
---
+++ the80by20/src/Shared/the80by20.Shared.Infrastucture/ExtensionsServiceCollection.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System.Runtime.CompilerServices;
+using the80by20.Shared.Abstractions.DomainLayer.SharedKernel;
+using the80by20.Shared.Infrastucture.Configuration;
+using the80by20.Shared.Infrastucture.Exceptions;
+using the80by20.Shared.Infrastucture.Time;
+
+[assembly: InternalsVisibleTo("the80by20.Bootstrapper")]
+namespace the80by20.Shared.Infrastucture
+{
+    internal static class ExtensionsServiceCollection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+
+            //var appOptions = configuration.GetOptions<AppOptions>(sectionName:"app");
+            //services.AddSingleton(appOptions);
+            services.Configure<AppOptions>(configuration.GetRequiredSection(key: "app"));
+
+            services.AddSingleton<ExceptionMiddleware>();
+            services.AddHttpContextAccessor();
+
+            services.AddSingleton<IClock, Clock>();
+
+            services.AddEndpointsApiExplorer(); // todo
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "The 80 by 20",
+                    Description = "system created from model to implementaion; model done used event storming, implemntaion done with ddd (strategic, tactical), architectural patterns based on modules drivers; high cohesion low coupling; tests; infrastructure"
+                });
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description =
+                        "Enter 'Bearer' [space] and then your valid token in the text input below." +
+                        "\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] { }
+                }
+                });
+            });
+
+            var appOptions = configuration.GetOptions<AppOptions>("app");
+
+            // INFO CORS configuration based on ms-docs: https://learn.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0
+            // Default Policy
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.WithOrigins(appOptions.FrontUrl)
+                                            .AllowAnyHeader()
+                                            .AllowAnyMethod();
+                    });
+            });
+
+            return services;
+        }       
+    }
+}
