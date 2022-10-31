56dd6300cb874d67c2a0d23c6f38a9fb9e2a88ea
simonconrad <szymon.kondratowicz@outlook.com>
2022-10-31 19:52:14 +0100

    .

--- the80by20/src/Bootstrapper/the80by20.Bootstrapper/ModuleLoader.cs
+++ the80by20/src/Bootstrapper/the80by20.Bootstrapper/ModuleLoader.cs
@@ -10,7 +10,8 @@ namespace the80by20.Bootstrapper
             var assemblies = AppDomain.CurrentDomain.GetAssemblies().ToList();
             var locations = assemblies.Where(x => !x.IsDynamic).Select(x => x.Location).ToArray();
             var files = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
-                 .Where(x => !locations.Contains(x, StringComparer.InvariantCultureIgnoreCase))
+                 .Where(x =>( x.Contains("Masterdata") || x.Contains("Solution") || x.Contains("Users")) // todo fix this hack, without <RuntimeIdentifier>win-x64</RuntimeIdentifier> in bootstrapper csproj this code is not needed as far more less files
+                 && !locations.Contains(x, StringComparer.InvariantCultureIgnoreCase))
                  .ToList();
 
             files.ForEach(x => assemblies.Add(AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(x))));
