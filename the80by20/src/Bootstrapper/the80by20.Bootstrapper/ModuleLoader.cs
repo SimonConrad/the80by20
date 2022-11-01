a2e80d5eb9580f1bd3bb0310ed5ecc0253203ba3
simonconrad <szymon.kondratowicz@outlook.com>
2022-11-01 08:38:38 +0100

    checkin if is assmebly by System.Reflection.Metadata;

--- the80by20/src/Bootstrapper/the80by20.Bootstrapper/ModuleLoader.cs
+++ the80by20/src/Bootstrapper/the80by20.Bootstrapper/ModuleLoader.cs
@@ -1,5 +1,7 @@
 ﻿using Serilog;
 using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 using the80by20.Shared.Abstractions.Modules;
 
 namespace the80by20.Bootstrapper
@@ -20,6 +22,16 @@ namespace the80by20.Bootstrapper
 
             AddIfAssmebly(assemblies, files);
 
+            foreach (var file in files)
+            {
+                if (IsAssembly(file))
+                {
+                    var assemblyName = AssemblyName.GetAssemblyName(file);
+                    var assembly = AppDomain.CurrentDomain.Load(assemblyName);
+                    assemblies.Add(assembly);
+                }
+            }
+
             return assemblies;
         }
 
@@ -38,6 +50,23 @@ namespace the80by20.Bootstrapper
                     continue;
                 }
             }
+        }
+
+        static bool IsAssembly(string path)
+        {
+            using var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+            // Try to read CLI metadata from the PE file.
+            using var peReader = new PEReader(fs);
+
+            if (!peReader.HasMetadata)
+            {
+                return false; // File does not have CLI metadata.
+            }
+
+            // Check that file has an assembly manifest.
+            MetadataReader reader = peReader.GetMetadataReader();
+            return reader.IsAssembly;
         }
 
         public static IList<IModule> LoadModules(IEnumerable<Assembly> assemblies)
